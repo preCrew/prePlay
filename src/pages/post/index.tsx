41979bb7bae136ded6e1fe2c10c896cb4fbce275
이챙 <cycy8527@gmail.com>
2022-09-19 12:13:32 +0900

    add: view page style
    completed

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,63 +1,49 @@
+import CommentList from '@src/components/Comment';
+import CommentForm from '@src/components/Comment/CommentForm';
+import Heart from '@src/components/Common/Button/Heart';
 import React from 'react';
 
 const Post = () => {
   return (
-    <div className="px-7">
-      <div className="flex justify-between w-4/5 py-8 m-auto">
+    <div>
+      <div className="flex justify-between py-8 m-auto px-7 md:w-4/5 ">
         <h2>playlist1/talk that talk-트와이스</h2>
-        <i>+30</i>
+        <Heart />
       </div>
-      <div className="max-w-[644px] m-auto">
+      <div className="max-w-[644px] m-auto px-7">
         <section className="mb-7">
-          <article className="bg-black">
+          <article className="bg-black rounded-b-xl">
             <div className="">
-              <img src="https://image.ytn.co.kr/general/jpg/2022/0805/202208050913233246_d.jpg" />
+              <img
+                className="object-cover w-full h-[unset]"
+                src="https://image.ytn.co.kr/general/jpg/2022/0805/202208050913233246_d.jpg"
+              />
             </div>
-            <div className="relative pb-6 mx-4 mt-6 text-white rounded-b-xl">
-              <h3 className="mb-5 text-xl">talk that talk</h3>
+            <div className="relative pb-6 mx-4 mt-6 text-white ">
+              <h3 className="w-11/12 mb-5 text-xl">
+                talk that talktalk that talktalk that talktalk that talktalk
+                that talktalk that talktalk that talktalk that talktalk that
+                talktalk that talk
+              </h3>
               <p className="mb-8 text-sm">트와이스</p>
-              <ul>
+              {/* <ul>
                 <li>
                   <a href="">#해시태그</a>
                 </li>
-              </ul>
+              </ul> */}
               <button
                 type="button"
-                className="absolute top-0 right-5"
+                className="absolute top-0 right-3"
               >
-                좋아요
+                <Heart />
               </button>
             </div>
           </article>
         </section>
         <section>
           <article>
-            <div className="px-4 pt-4 pb-8 border rounded-lg border-primary1">
-              <div>
-                <span>cdfkjdlfkgjdf@gmail.com</span>
-              </div>
-              <form className="relative">
-                <input
-                  type="text"
-                  placeholder="댓글을 달아주세요"
-                  className="w-[calc(100%-60px)]"
-                />
-                <button className="w-[50px] absolute right-0 top-0 bg-[#d2d6f8] text-sm">
-                  전송
-                </button>
-              </form>
-            </div>
-            <ul>
-              <li>
-                <div>
-                  <span>cdfkjdlfkgjdf@gmail.com</span>
-                  <sub>2022.09.10</sub>
-                </div>
-                <p>
-                  정말노래좋아여정말노래좋아여정말노래좋아여정말노래좋아여정말노래좋아여정말노래좋아여정말노래좋아여정말노래좋아여정말노래좋아여정말노래좋아여정말노래좋아여
-                </p>
-              </li>
-            </ul>
+            <CommentForm />
+            <CommentList />
           </article>
         </section>
       </div>
