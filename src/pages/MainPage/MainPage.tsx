89cfb1bd0cc00e59e6c99c346f118cad38d70474
ChoiSunAh, 최선아 <csa2676@naver.com>
2022-09-26 20:48:18 +0900

    docs: 안 쓰는 import 정리

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import heartF from '@src/assets/heartFlat.png';
 
 interface MainPageProps {}
