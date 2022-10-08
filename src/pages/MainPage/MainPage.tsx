e73821a2ae3c7b8508173b71732236bb35e1a2ac
이챙 <cycy8527@gmail.com>
2022-10-08 20:27:16 +0900

    Add: muscLists를 state값에 저장

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,14 +1,19 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import useGetSongCountQuery from '@src/hooks/useGetSongCountQuery';
-import useGetSongListsQuery from '@src/hooks/useGetSongListsQuery';
+import useGetSongListsQuery, {
+  TSongLists,
+} from '@src/hooks/useGetSongListsQuery';
 import useInfinityScroll from '@src/hooks/useInfitityScroll';
 import SkeletonMainPage from '@src/components/MainPage/SkeletonMainPage';
 import MainBox from '@src/components/MainPage/MainBox';
 import { useRef } from 'react';
+import { getPostAction, TypefinalPosts } from '@src/store/reducers/post-Slice';
+import { useAppDispatch } from '@src/store/store';
 
 interface MainPageProps {}
 
 const MainPage = ({}: MainPageProps) => {
+  const dispatch = useAppDispatch();
   const { data: musicCnt } = useGetSongCountQuery();
   const next = useRef(0);
   const musicsOrder = useMemo(() => {
@@ -33,9 +38,17 @@ const MainPage = ({}: MainPageProps) => {
     fetchNextPage();
   });
 
+  useEffect(() => {
+    if (musicLists) {
+      dispatch(getPostAction({ getPosts: musicLists.pages[0].pageLists }));
+    }
+
+    //console'TypefinalPosts.log();
+  }, [dispatch, musicLists]);
+
   return (
     <>
-      <div className="grid justify-center w-[100%] pt-[19px]  bg-gray-600 gap-x-[27px] gap-y-[30px] grid-cols-auto-fit justify-items-center pr-[48px] pl-[39px] min-w-fit">
+      <div className="grid justify-center w-[100%] pt-[19px]  bg-gray-600 gap-x-[27px] gap-y-[30px] grid-cols-auto-fit justify-items-center min-w-fit">
         {musicLists &&
           musicLists.pages.map(page =>
             page.pageLists.map(item => <MainBox item={item} />),
