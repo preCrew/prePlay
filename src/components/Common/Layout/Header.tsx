d52fe0f9442cbf2125464a66a5cdc9c9e1da6cb7
이챙 <cycy8527@gmail.com>
2023-04-25 23:22:44 +0900

    #28 Add: header and nav
    - 헤더
    - 하단 nav바 생성

--- src/components/Common/Layout/Header.tsx
+++ src/components/Common/Layout/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import user from '@src/assets/user.png';
-import search from '@src/assets/search.png';
+
 import classNames from 'classnames';
 import { Link, useNavigate } from 'react-router-dom';
 import Search from '../../../pages/SearchPage';
@@ -21,107 +20,23 @@ const Header = () => {
     navigate(`/search`, { state: { search_KW: search_KW } });
   };
   return (
-    <header>
-      <nav className="relative mx-7 min-w-fit">
-        <div className="flex items-center pt-3 m-3 space-between ">
-          {/* mobile hamburger S*/}
-          <div className="absolute items-center space-x-4 lg:hidden">
-            <button
-              className="inline-flex ml-auto rounded outline-none lg:hidden"
-              onClick={() => setMenuToggle(!menuToggle)}
-            >
-              {menuToggle ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </button>
-          </div>
-          {/* mobile hamburger E*/}
-
-          {/* LOGO */}
-          <div className="flex flex-row flex-wrap items-center justify-center w-full px-6 space-x-6 ">
-            <div className="flex flex-row justify-center ">
-              <div className="font-[Monoton] text-[#787bc5] text-[28px] ">
-                PREPLAYER
-              </div>
+    <header className="fixed top-0 left-0 z-50 w-full bg-white ">
+      <div className="flex items-center m-3 3 space-between ">
+        {/* LOGO */}
+        <div className="flex flex-row flex-wrap items-center justify-center w-full px-6 space-x-6 ">
+          <div className="flex flex-row justify-center ">
+            <div className="font-[Monoton] text-[#787bc5] text-[15px] ">
+              PREPLAYER
             </div>
           </div>
-
-          {/* PC Menu items */}
-          <div className="w-full absolute top-[33px] left-[3%]">
-            <Link to="/login">
-              <button className=" border-0 py-3 invisible lg:visible leading-none text-xl bg-transparent mr-2  w-[15px]">
-                <img
-                  src={user}
-                  alt="user"
-                ></img>
-              </button>
-            </Link>
-
-            <button
-              onClick={SearchonClick}
-              className="ml-[34px] border-0 py-3 invisible lg:visible leading-none text-xl bg-transparent mr-2  w-[15px]"
-            >
-              <img
-                src={search}
-                alt="search"
-              ></img>
-            </button>
-
-            <input
-              className="ml-[8px] text-[15px] font-[Inter] invisible lg:visible "
-              type="text"
-              placeholder="SEARCH FOR MUSIC TAG"
-              onChange={onChange}
-              onKeyPress={onKeyPress}
-            />
-          </div>
         </div>
-      </nav>
 
-      {/* mobile menu items */}
-      <div className={classNames({ hidden: !menuToggle })}>
-        <>
-          <Link
-            to="/Login"
-            className="block px-4 py-2 hover:bg-gray-200"
-          >
-            User
-          </Link>
-          <Link
-            to="#"
-            className="block px-4 py-2 text-sm hover:bg-gray-200"
-          >
-            Search
-          </Link>
-        </>
+        <div className="w-full absolute top-[33px] left-[3%]">
+          {/* <button
+              onClick={SearchonClick}
+              className="ml-[34px] border-0 py-3 leading-none text-xl bg-transparent mr-2  w-[15px]"
+            ></button> */}
+        </div>
       </div>
     </header>
   );
