e2a7325f75a6941af4f289a381f25c1277fa1587
이챙 <cycy8527@gmail.com>
2023-05-11 12:25:06 +0900

    fix: build issue

--- src/components/common/Button/LoginButton/LoginButton.tsx
+++ src/components/common/Button/LoginButton/LoginButton.tsx
@@ -1,3 +1,5 @@
+import { FcGoogle } from 'react-icons/fc';
+
 interface LoginButtonProps {
   onClick?: () => void;
 }
@@ -8,11 +10,8 @@ const LoginButton = ({ onClick }: LoginButtonProps) => {
       onClick={onClick}
       className="flex items-center justify-center w-11/12 p-3 border border-gray-200 rounded-3xl"
     >
-      <img
-        className="w-[18px] mr-[18px]"
-        src="./src/assets/images/google.png"
-      />
-      <span>구글로 로그인 하기</span>
+      <FcGoogle fontSize="25px" />
+      <span className="ml-2">구글로 로그인 하기</span>
     </button>
   );
 };
